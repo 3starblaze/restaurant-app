5e178c35d4d697b4a22e8e7c07eb20fa31dc3cd1
Deniss Fjodorovs <3starblaze@gmail.com>
2021-07-09 08:57:42 +0300

    DRY up map code

---
+++ resources/views/components/map.blade.php
@@ -0,0 +1,19 @@
+@props(['lat' => 56.9566, 'lng' => 24.1315, 'zoom' => 13])
+
+<div id="map" class="h-80 w-full" {{ $attributes }}></div>
+
+<script type="text/javascript">
+ (() => {
+     const map = L.map('map').setView([{{ $lat }}, {{ $lng }}], {{ $zoom }});
+     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+         maxZoom: 18,
+         id: 'mapbox/streets-v11',
+         tileSize: 512,
+         zoomOffset: -1,
+         accessToken: '{{ env('MAP_PUBLIC_TOKEN')  }}',
+     }).addTo(map);
+
+     {{ $slot }}
+ })();
+</script>
