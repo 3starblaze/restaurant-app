7df3a07f475b0f690a4dd5283fa59ff1d28466b6
3starblaze <3starblaze@protonmail.com>
2021-08-06 09:09:40 +0300

    Create reservation factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -18,18 +20,31 @@ class DatabaseSeeder extends Seeder
     {
         // For now it's just a normal user
         if (! User::where('email', 'admin@admin.com')->first()) {
-            $adminUser = \App\Models\User::factory()->create([
+            $adminUser = User::factory()->create([
                 'name' => 'Admin',
                 'email' => 'admin@admin.com',
                 'password' => Hash::make('admin'),
             ]);
 
-            \App\Models\Restaurant::factory()->create([
+            Restaurant::factory()->create([
                 'user_id' => $adminUser,
                 'name' => 'Adminifoods',
             ]);
+
+            // Can't use factory output. restaurant does not have an ID there
+            $restaurant = $adminUser->restaurant;
+
+            Reservation::factory(10)->create([
+                'restaurant_id' => $restaurant,
+            ]);
         }
 
-        \App\Models\Restaurant::factory(10)->create();
+        $restaurants = Restaurant::factory(10)->create();
+        $restaurants = $restaurants->fresh();
+        foreach($restaurants as $restaurant) {
+            Reservation::factory(10)->create([
+                'restaurant_id' => $restaurant,
+            ]);
+        }
     }
 }
