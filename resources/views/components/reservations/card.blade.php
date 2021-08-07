e8912b906c93620db58a1634dd8fa2c679d4d4cc
3starblaze <3starblaze@protonmail.com>
2021-08-07 09:43:03 +0300

    Improve restaurant.show reservations look
    This commit ensures that restaurant.show keeps looking sensible in
    different breakpoints. Also includes small modifications to
    reservations.card for customizeability.

--- resources/views/components/reservations/card.blade.php
+++ resources/views/components/reservations/card.blade.php
@@ -1,9 +1,9 @@
-<div class="ml-5 mt-5 max-w-xl">
-    <h2 class="font-bold">
+<div {{ $attributes->merge(['class' => 'max-w-xl']) }}>
+    <x-h2>
         <x-a href="{{ route('reservations.show', compact('reservation')) }}">
-            {{ $reservation->start_time }} - {{ $reservation->end_time }}
+    {{ $reservation->start_time }} - {{ $reservation->end_time }}
         </x-a>
-    </h2>
+    </x-h2>
     <p>{{ __('Guest count') }}: {{ $reservation->max_person_count }}</p>
     <p>{{ __('Description') }}: {{ $reservation->description }}</p>
 </div>
--- resources/views/restaurant/show.blade.php
+++ resources/views/restaurant/show.blade.php
@@ -12,28 +12,31 @@
         </a>
         @endcan
     </x-slot>
-    <div class="flex flex-col sm:flex-row">
-        <p class="max-w-xl w-full sm:flex-1">{{ $restaurant->description }}</p>
+    <div class="flex flex-col lg:flex-row lg:gap-10">
+        <div class="flex flex-col sm:flex-row lg:flex-col">
+            <p class="max-w-xl w-full sm:flex-1 lg:flex-none">{{ $restaurant->description }}</p>
 
-        <div class="sm:flex-1">
-            <!-- Style attribute makes the map a perfect square -->
-            <x-map style="height:0;width:100%;padding-bottom:100%;"
-                   :lat="$restaurant->latitude"
-                   :lng="$restaurant->longitude"
-                   zoom="close">
-                L.marker({
+            <div class="sm:flex-1">
+                <!-- Style attribute makes the map a perfect square -->
+                <x-map style="height:0;width:100%;padding-bottom:100%;"
+                       :lat="$restaurant->latitude"
+                       :lng="$restaurant->longitude"
+                       zoom="close">
+                    L.marker({
                     lat: {{ $restaurant->latitude }},
                     lng: {{ $restaurant->longitude }},
-                }).addTo(map);
-            </x-map>
+                    }).addTo(map);
+                </x-map>
+            </div>
         </div>
-    </div>
-    <x-h2>{{ __('Available reservations') }}</x-h2>
-    <div>
-        <ul>
-            @foreach ($restaurant->reservations as $reservation)
-                <x-reservations.card :reservation="$reservation" />
-            @endforeach
-        </ul>
+        <div>
+            <x-h2 class="mt-5">{{ __('Available reservations') }}</x-h2>
+            <ul>
+                @foreach ($restaurant->reservations as $reservation)
+                    <x-reservations.card :reservation="$reservation"
+                                         class="mt-3"/>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </x-app-layout>
