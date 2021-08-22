bf88b2a7e7d59efa6c332f0402a96719ca512d89
3starblaze <3starblaze@protonmail.com>
2021-08-22 12:34:26 +0300

    Make restaurant popup card united
    A new index card component is made for reuse which is now used by
    restaurant popup.

--- resources/views/restaurant/index.blade.php
+++ resources/views/restaurant/index.blade.php
@@ -48,39 +48,7 @@
 
     <x-base class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
         @forelse ($restaurants as $restaurant)
-            <x-card>
-                <div class="p-2">
-                    <div class="flex justify-between">
-                        <div>
-                            <h2 class="font-bold text-lg">
-                                {{ $restaurant->name }}
-                            </h2>
-                            <!-- Flex for removing implicit whitespace -->
-                            <!-- Shift div for finer alignment -->
-                            <div class="flex relative -left-0.5">
-                                @php
-                                $starCount = rand(1, 5);
-                                @endphp
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($i <= $starCount)
-                                        <x-bare.star-icon
-                                            class="h-5 inline-block text-yellow-500" />
-                                    @else
-                                        <x-bare.star-icon
-                                            class="h-5 inline-block text-yellow-200" />
-                                    @endif
-                                @endfor
-                            </div>
-                        </div>
-                        <x-arrow-button :href="route('restaurant.show', compact('restaurant'))"
-                                        :text="__('View')" />
-                    </div>
-                </div>
-                <p class="px-2 mb-2">{{ $restaurant->description }}</p>
-                <img src="/test-images/restaurants/00{{ rand(0, 5) }}.jpg"
-                     alt="restaurant"
-                     class="rounded-b" />
-            </x-card>
+            <x-restaurant.index-card :restaurant="$restaurant" />
         @empty
             <p>No restaurants yet.</p>
         @endforelse
