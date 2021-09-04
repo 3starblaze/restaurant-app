1eeaea4a66056bed49084885d12505935b5d578e
3starblaze <3starblaze@protonmail.com>
2021-09-04 09:59:32 +0300

    Fix alpine.js error

--- resources/views/components/a.blade.php
+++ resources/views/components/a.blade.php
@@ -1,4 +1,4 @@
-<a :href="$href"
+<a href="{{ $href }}"
    {{ $attributes->merge(['class' => implode(' ', [
        'inline-block', // To trim extra whitespace around the link
        'text-blue-900',
